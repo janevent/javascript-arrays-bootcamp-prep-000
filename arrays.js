6020bcdbf826690f04450d726e866a7045311b23
Jane Venter <janeanneventer@gmail.com>
2018-08-26 04:51:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,6 +17,6 @@ function accessElementInArray(array, index){
 function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift();
 }
-removeElementFromBeginningOfArray(array){
+function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
