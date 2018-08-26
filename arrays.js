e5da43414b54066a193c1a2fad7fbacbdc4b29e3
Jane Venter <janeanneventer@gmail.com>
2018-08-26 05:42:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,11 +3,11 @@ function addElementToBeginningOfArray(array, element){
   return [element, ... array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
 function addElementToEndOfArray(array, element){
-  [... array, element];
-  return array;
+  return [... array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element){
   array.push(element);
