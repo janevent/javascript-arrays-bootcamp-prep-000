dcbe44dc776142f1b9cbc666183d12e9ecaa589f
Jane Venter <janeanneventer@gmail.com>
2018-08-26 04:24:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,12 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+const array = [1];
 function addElementToBeginningOfArray(array, element){
   return [element, ... array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element);
 }
+destructivelyAddElementToBeginningOfArray(array, 'foo');
 function addElementToEndOfArray(array, element){
   return [... array, element];
 }
